3ad9de03b55c04046fb11ed91b3d02c4a08ee807
Boris Kachscovsky <kachscovsky@gmail.com>
2014-01-16 21:48:52 +0100

    parser finished, needs sync

--- parser.hs
+++ parser.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 import Test.HUnit
+import Data.Foldable
 import Control.Monad
+import System.Environment
 import Test.QuickCheck
 import Test.QuickCheck.All
 import Debug.Trace
+import Data.Array
 import Data.List
+import Data.Sequence
 
 data Tile = Empty | X | O
 instance Show Tile where
@@ -12,21 +16,41 @@ instance Show Tile where
     show (X) = "X"
     show (O) = "O"
 
-lineToReadInt :: IO[Int]
-lineToReadInt = (map read . words) `fmap` getLine
-
-lineToReadStr:: IO[String]
-lineToReadStr = (lines) `fmap` getLine
-
-main :: IO ()
-main = do
-    ln <- lineToReadInt
-    let rws = ln !! 0
-        cls = ln !! 1
-    test <- readLine(cls)
-    [test] <- lineToReadStr
-    print $ map toTile test
+instance Eq Tile where
+    X == X = True
+    Empty == Empty = True
+    O == O = True
+    _ == _  = False
 
 toTile '.' = Empty
 toTile 'X' = X
 toTile 'O' = O
+toTile p = error [p]
+
+
+readWords :: (Read a) => String -> [a]
+readWords = map read . words
+lineToReadInt :: IO[Int]
+lineToReadInt = (readWords) `fmap` getLine
+lineToReadStr :: IO[String]
+lineToReadStr = (lines) `fmap` getLine
+
+getBoard = do
+    ln <- lineToReadInt
+    let rws = ln !! 0
+        cls = ln !! 1
+    board <- Control.Monad.replicateM rws readLine
+    return board
+
+if' :: Bool -> a -> a -> a
+if' True  x _ = x
+if' False _ y = y
+
+readLine = (map toTile) `fmap` getLine
+
+writeTile board rw cl new = toList (update rw (getRow board rw cl new) $ fromList board)
+getRow (x:xs) 0 cl new = toList (getColumn (fromList x) cl new)
+getRow (x:xs) rw cl new = getRow xs (rw-1) cl new
+getColumn board cl new = update cl new $ board
+
+readTile board x y = board !! x !! y
