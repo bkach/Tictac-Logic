c1b88b927e4bf22801a422004878314d0e387b73
Alan Ghobadi <alan@tictail.com>
2014-01-19 18:18:01 +0100

    Commented solver.hs

--- Solver.hs
+++ Solver.hs
@@ -6,6 +6,11 @@ import Tile
 import Data.Maybe
 import Data.List
 
+-------------------------------------------------------------------------------
+-- adjTwins board coord - Rule that checks if Tile at cordinate coord has a pair
+-- of equal neighbours and returns the opposite of that.
+-- Example .OO => X
+-------------------------------------------------------------------------------
 adjTwins :: [[Tile]] -> (Int, Int) -> Maybe Tile
 adjTwins board coord
     | inBounds board (leftN 2 coord) &&
@@ -26,6 +31,10 @@ adjTwins board coord
                 Just (opposite (downTile board coord))
     | otherwise = Nothing
 
+-------------------------------------------------------------------------------
+-- coundElemCol board coord tile - Counts the number of tiles that are equal
+-- to t in a column, elemFilled helper
+-------------------------------------------------------------------------------
 countElemCol :: (Eq a1, Num a) => [[a1]] -> (Int, Int) -> a1 -> a
 countElemCol board (i,j) t
     | i >= (rows board) = 0
@@ -35,6 +44,10 @@ countElemCol board (i,j) t
         coord = (i,j)
         tile = readTile board coord
 
+-------------------------------------------------------------------------------
+-- coundElemRow board coord tile - Counts the number of tiles that are equal
+-- to t in a row, elemFilled helper
+-------------------------------------------------------------------------------
 countElemRow :: (Eq a1, Num a) => [[a1]] -> (Int, Int) -> a1 -> a
 countElemRow board (i,j) t
     | j >= (columns board) = 0
@@ -44,6 +57,10 @@ countElemRow board (i,j) t
         coord = (i,j)
         tile = readTile board coord
 
+-------------------------------------------------------------------------------
+-- elemFilled board coord - Rule to check and see if it is possible to fill a
+-- tile by mutual exclusion. That is, a row or column already have enough O:s or X:s
+-------------------------------------------------------------------------------
 elemFilled :: [[Tile]] -> (Int, Int) -> Maybe Tile
 elemFilled board (i,j)
     | countElemCol board (0, j) X == halfCol = Just O
@@ -55,6 +72,10 @@ elemFilled board (i,j)
         halfCol = (rows board +1)`div` 2
         halfRow = (columns board +1) `div` 2
 
+-------------------------------------------------------------------------------
+-- inBetween board coord - Rule to check if an empty tile is between to similar
+-- tiles, and returns the opposite of that. Example X.X => O
+-------------------------------------------------------------------------------
 inBetween :: [[Tile]] -> (Int, Int) -> Maybe Tile
 inBetween board coord
     | inBounds board (left coord) &&
@@ -69,6 +90,10 @@ inBetween board coord
                     Just (opposite (upTile board coord))
     | otherwise = Nothing
 
+-------------------------------------------------------------------------------
+-- checkTile board coord - Tries to returns what to fill the tile at coord with
+-- by checking the above rules
+-------------------------------------------------------------------------------
 checkTile :: [[Tile]] -> (Int, Int) -> Maybe Tile
 checkTile board coord
     | readTile board coord /= Empty = Nothing
@@ -81,6 +106,9 @@ checkTile board coord
         adj = adjTwins board coord
         half = elemFilled board coord
 
+-------------------------------------------------------------------------------
+-- findEmptyIndex board coord - Finds the index of the first empty tile in board
+-------------------------------------------------------------------------------
 findEmptyIndex :: [[Tile]] -> (Int, Int) -> (Int, Int)
 findEmptyIndex board coord
     | (readTile board coord) == Empty = coord
@@ -88,8 +116,17 @@ findEmptyIndex board coord
     | otherwise = findEmptyIndex board nextTile
     where nextTile = iterateTile board coord
 
+-------------------------------------------------------------------------------
+-- fillFirstEmpty board t - Sets the first empty tile in board with t
+-------------------------------------------------------------------------------
 setFirstEmpty :: [[Tile]] -> Tile -> [[Tile]]
 setFirstEmpty board t = writeTile board (findEmptyIndex board (0,0)) t
+
+-------------------------------------------------------------------------------
+-- solve' board coord changed - Goes through entire board once and tries to
+-- fill each tile by applying rules. Returns the new board and if it succeded
+-- at setting a tile in the board.
+-------------------------------------------------------------------------------
 solve' :: [[Tile]] -> (Int, Int) -> Bool -> (Bool, [[Tile]])
 solve' board coord changed
     | tileResult /= Nothing =
@@ -101,6 +138,11 @@ solve' board coord changed
         tileResult = checkTile board coord
         nextTile = iterateTile board coord
 
+-------------------------------------------------------------------------------
+-- solve [board] - Tries to solve each board by using solve' (rule based),
+-- otherwise starts guessing elements and and keep trying. This is done in a
+-- DFS way. Returns solved board or throws an error.
+-------------------------------------------------------------------------------
 solve :: [[[Tile]]] -> [[Tile]]
 solve [] = error "No solution found!"
 solve (board:boards)
@@ -115,19 +157,27 @@ solve (board:boards)
         (changed, newBoard) = solve' board (0,0) False
         full = filled newBoard
 
+-------------------------------------------------------------------------------
+-- checkRow row rows - checkRows helper, compares row against all elements in rows
+-------------------------------------------------------------------------------
 checkRow :: Eq a => a -> [a] -> Bool
 checkRow row [] = True
 checkRow row (x:xs)
     | row == x = False
     | otherwise = checkRow row xs
 
+-------------------------------------------------------------------------------
+-- checkRows rows - validate helper
+-------------------------------------------------------------------------------
 checkRows :: Eq t => [t] -> Bool
 checkRows [] = True
 checkRows (x:xs)
     | not check = False
     | otherwise = checkRows xs
     where check = checkRow x xs
-
+-------------------------------------------------------------------------------
+-- validate board - Checks if pairs of rows and columns are unique
+-------------------------------------------------------------------------------
 validate :: Eq a => [[a]] -> Bool
 validate board
     | cols && rows = True
