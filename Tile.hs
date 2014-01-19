e19a860656aed7954358d9c5a7e516bf6000101f
Alan Ghobadi <alan@tictail.com>
2014-01-19 18:09:08 +0100

    Commented Tile.hs

--- Tile.hs
+++ Tile.hs
@@ -13,11 +13,17 @@ instance Eq Tile where
     O == O = True
     _ == _  = False
 
+-------------------------------------------------------------------------------
+-- toTile c - Maps Char to corresponing Tile
+-------------------------------------------------------------------------------
 toTile '.' = Empty
 toTile 'X' = X
 toTile 'O' = O
 toTile p = error [p]
 
+-------------------------------------------------------------------------------
+-- opposite t - Returns the opposite of a Tile
+-------------------------------------------------------------------------------
 opposite X = O
 opposite O = X
 opposite Empty = Empty
